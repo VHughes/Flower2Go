4e4e38d6c91d7f7bb0ae50798ac712b0609b038e
VHughes <virginia.hughes04@gmail.com>
2016-03-25 21:31:47 -0700

    dicking around cause its what i do

--- controller/products.php
+++ controller/products.php
@@ -11,6 +11,7 @@ loadScripts();
 
         $html = "";
         foreach($rows as $row) {
+   
             
             $sku = $row['SKU'];
             $title = $row['title'];
@@ -19,12 +20,15 @@ loadScripts();
             $qty = $row['quantity'];
             $html .= "<tr id='ShoppingCartTr'>
                         <td data-sku-title='$sku'>$title</td>
-                        <td data-sku-desc='$sku'>$desc</td>
-                        <td><input data-sku-qty='$sku' type='number' value='1' min='1' max='10' step='1'/></td>
+                        <td data-sku-desc='$sku'>$desc</td> 
+                        <td><input data-sku-qty='$sku' type='number' id='qty' value='1' min='1' max='10' step='1'/></td>
                         <td data-sku-price='$sku'>$price</td>
                         <td data-sku-qty='$sku'>$qty</td>
                         <td><input id='addBut' data-sku-add='$sku' type='button' value='+'/></td>
                       </tr>";
+            
+             
+      
         }
         echo $html;
         return;
